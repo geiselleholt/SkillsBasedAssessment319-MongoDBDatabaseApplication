64c44aac1b57178493ced78cfd9db9e3ce3d8415
Geiselle Holt <geiselleholt@hotmail.com>
2025-05-02 00:31:35 -0500

    added indexing to userSchema

--- models/userSchema.mjs
+++ models/userSchema.mjs
@@ -6,10 +6,12 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     maxLength: 30,
+    index: true,
   },
   destination: {
     type: String,
     required: true,
+    index: true,
   },
   child: {
     type: Boolean,
@@ -24,7 +26,10 @@ const userSchema = new mongoose.Schema({
       },
       message: "Did you already go on this trip? Your trip date is in the past",
     },
+    index: true,
   },
 });
+
+userSchema.index({ destination: 1, tripDate: -1 });
 
 export default mongoose.model("User", userSchema);
